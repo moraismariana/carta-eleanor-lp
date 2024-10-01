cd8b9357be3f91cbbe7b322fd7d3843003baed4c
moraismariana <moraismariana200@gmail.com>
2024-10-01 15:36:19 -0300

    Responsividade

--- script.js
+++ script.js
@@ -14,6 +14,18 @@ const splideOptions = {
   type: "loop",
   fixedWidth: "62.5%",
   gap: "28px",
+
+  breakpoints: {
+    1024: {
+      fixedWidth: "80%",
+      gap: "20px",
+    },
+    480: {
+      fixedWidth: "95%",
+      gap: "20px",
+      arrows: false,
+    },
+  },
 };
 
 new Splide(".splide", splideOptions).mount();
